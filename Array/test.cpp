5a7bf7768dfc22ae3353e3147931f26fac7492b4
fLexsooP <chenceshi@Chence-MacBook.home>
2022-06-07 19:07:40 -0700

    update

--- Array/test.cpp
+++ Array/test.cpp
@@ -5,15 +5,6 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    int f, g;
-    for (size_t i = 0; i < 5; i++)
-    {
-        if (f == g)
-        {
-            f -> g;
-            f >= g;//
-            1000
-        }
-    }
+    
     return 0;
 }
