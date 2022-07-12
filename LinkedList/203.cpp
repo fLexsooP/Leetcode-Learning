fca4ae8630bb326c1af40b4967c9872d082d8cec
fLexsooP <ascc594557067@gmail.com>
2022-07-11 21:52:17 -0700

    finish up 203

--- LinkedList/203.cpp
+++ LinkedList/203.cpp
@@ -1,14 +1,42 @@
 #include <iostream>
 
-struct ListNode {
-    int val;
-    ListNode* next;
-    ListNode(int x) : val(x), next(NULL) {}
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    struct ListNode {
+     int val;
+     ListNode* next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+    
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummyHead = new ListNode(0);
+        dummyHead->next = head;
+        ListNode* cur = dummyHead;
+
+        while(cur->next != nullptr) {
+            if(cur->next->val == val) {
+                ListNode* temp = cur->next;
+                cur->next = cur->next->next;
+                delete temp;
+            } else {
+                cur = cur->next;
+            }
+        }
+        head = dummyHead->next;
+        delete dummyHead;
+        return head;
+    }
 };
 
-
-int main(int argc, char const *argv[])
-{
-    Listnode 
-    return 0;
-}
