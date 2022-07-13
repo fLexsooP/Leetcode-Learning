5a590af81d345d83fb2d913ce05b0e2283c59678
fLexsooP <chenceshi@Chence-MacBook.home>
2022-07-13 15:11:08 -0700

    92 completed

--- LinkedList/92.cpp
+++ LinkedList/92.cpp
@@ -18,6 +18,21 @@ public:
         ListNode(int x, ListNode *next) : val(x), next(next) {}
     };
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        
+        if(!head || left >= right) {
+            return head;
+        }
+        ListNode* dummyHead = new ListNode(0, head);
+        ListNode* prev = dummyHead;
+        for (int i = 0; i < left - 1; i++) {
+            prev = prev->next;
+        }
+        ListNode* tail = prev->next;
+        for(int i = left; i < right; i++) {
+            ListNode* cache = tail->next;
+            tail->next = cache->next;
+            cache->next = prev->next;
+            prev->next = cache;
+        }
+        return dummyHead->next;
     }
 };
