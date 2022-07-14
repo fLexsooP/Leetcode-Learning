cf5a6605eff7337902e1378c0bac179e28ff5416
fLexsooP <ascc594557067@gmail.com>
2022-07-14 03:42:25 -0700

    complete 24

--- LinkedList/24.cpp
+++ LinkedList/24.cpp
@@ -19,5 +19,15 @@ public:
     };
     ListNode* swapPairs(ListNode* head) {
         
+        ListNode* dummyHead = new ListNode(0, head);
+        ListNode* prev = dummyHead;
+        while(prev->next && prev->next->next) {
+            ListNode* cache = prev->next->next;
+            prev->next->next = cache->next;
+            cache->next = prev->next;
+            prev->next = cache;
+            prev = cache->next;
+        }
+        return dummyHead->next;
     }
 };
