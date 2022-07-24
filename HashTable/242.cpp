e13deb5ba01f0202aa27eb9357ddfed0ee20a09f
fLexsooP <ascc594557067@gmail.com>
2022-07-24 02:02:29 -0700

    create 242

---
+++ HashTable/242.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    Solution sol = Solution();
+    cout << sol.isAnagram("nam", "man") << endl;
+    return 0;
+}
