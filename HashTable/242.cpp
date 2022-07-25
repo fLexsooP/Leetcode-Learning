7d7f7cc324f7a2c2dda6800f4af295d8a3d3e4ec
fLexsooP <ascc594557067@gmail.com>
2022-07-25 04:09:16 -0700

    complete 242

--- HashTable/242.cpp
+++ HashTable/242.cpp
@@ -1,16 +1,30 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        
+        if(s.size() != t.size())
+            return false;
+        vector<int> count(128);
+        for(const int c : s) {
+            ++count[c];
+        }
+        for(const int c : t) {
+            if(--count[c] < 0) {
+                return false;
+            }
+        }
+        return true;
     }
 };
 
 int main(int argc, char const *argv[])
 {
     Solution sol = Solution();
+    cout << sol.isAnagram("namm", "man") << endl;
+    cout << sol.isAnagram("nan", "man") << endl;
     cout << sol.isAnagram("nam", "man") << endl;
     return 0;
 }
