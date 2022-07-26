be72dc73b2abfb966be78f04a83ba2631cde34fd
fLexsooP <ascc594557067@gmail.com>
2022-07-26 00:27:49 -0700

    complete 349

--- HashTable/349.cpp
+++ HashTable/349.cpp
@@ -1,10 +1,18 @@
 #include<iostream>
 #include<vector>
+#include<unordered_set>
 using namespace std;
 
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        
+        vector<int> ans;
+        unordered_set<int> nums(nums1.begin(), nums1.end());
+        for (auto const num : nums2) {
+            if(nums.erase(num)) {
+                ans.push_back(num);
+            }
+        }
+        return ans;
     }
 };
