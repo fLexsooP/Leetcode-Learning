026fcfde6cda22f070f69b78a29547d34fcc7b73
fLexsooP <ascc594557067@gmail.com>
2022-07-26 01:04:22 -0700

    complete 202

--- HashTable/202.cpp
+++ HashTable/202.cpp
@@ -4,6 +4,29 @@ using namespace std;
 class Solution {
 public:
     bool isHappy(int n) {
-        
+        int slow = getSum(n);
+        int fast = getSum(getSum(n));
+        while(slow != fast) {
+            slow = getSum(slow);
+            fast = getSum(getSum(fast));
+        }
+        return slow == 1;
+    }
+
+    int getSum(int n) {
+        int sum = 0;
+        while(n) {
+            sum += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return sum;
     }
 };
+
+int main(int argc, char const *argv[])
+{
+    Solution sol;
+    cout << sol.isHappy(19) << endl;
+    cout << sol.isHappy(2) << endl;
+    return 0;
+}
