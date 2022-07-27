c52ecf596aa3b3dc306326328623fb088611c3cd
fLexsooP <ascc594557067@gmail.com>
2022-07-26 17:16:50 -0700

    complete 1

--- HashTable/1.cpp
+++ HashTable/1.cpp
@@ -1,11 +1,18 @@
 #include<iostream>
 #include<vector>
+#include<unordered_map>
 using namespace std;
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        unordered_map<int, int> umap; // array value as map key, array index as map value 
+        for (int i = 0; i < nums.size(); i++) {
+            if(umap.count(target - nums[i])) {
+                return {umap[target - nums[i]], i};
+            }
+            umap.insert({nums[i], i});
+        }
     }
 };
 int main(int argc, char const *argv[])
