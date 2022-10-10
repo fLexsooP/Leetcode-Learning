dcf7aa7ca1a8004786fb21ccc2452a1f06664461
fLexsooP <ascc594557067@gmail.com>
2022-10-10 04:00:10 -0700

    add 1209 in py

---
+++ Stack and Queue/1209.py
@@ -0,0 +1,17 @@
+from itertools import count
+
+
+class Solution:
+    def removeDuplicates(self, s: str, k: int) -> str:
+        stack = []
+        for c in s:
+            if not stack or stack[-1][0] != c:
+                stack.append([c, 1])
+            else:
+                stack[-1][1] += 1
+                if stack[-1][1] == k:
+                    stack.pop()
+        return "".join(c * count for c, count in stack)
+    
+s = Solution()
+print(s.removeDuplicates("aabbbavccvb", 2))
