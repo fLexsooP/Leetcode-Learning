1f6a0420f10f1c3e8784010137d2c3e8dc083a87
fLexsooP <ascc594557067@gmail.com>
2022-10-26 19:39:12 -0700

    modify 144, 145 and 94

--- BinaryTree/144_145_94.cpp
+++ BinaryTree/144_145_94.cpp
@@ -32,3 +32,28 @@ public:
         traversal(cur->right, res);
     }
 };
+
+int main(int argc, char const *argv[])
+{
+    TreeNode node = TreeNode(1);
+    node.left = new TreeNode(2);
+    node.right = new TreeNode(3);
+    node.left->left = new TreeNode(4);
+    node.left->right = new TreeNode(5);
+    node.right->left = new TreeNode(6);
+    node.right->right = new TreeNode(7);
+    /*
+                1
+        2               3
+    4       5       6       7
+    */
+
+    Solution s = Solution();
+    vector<int> vec = s.preorderTraversal(&node);
+    for (auto &&n : vec)
+    {
+        cout << n;
+    }
+    
+    return 0;
+}
