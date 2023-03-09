91bd6c23f780d13cfc819ea67a789b4c30dfdbb0
Chence Shi <ascc594557067@gmail.com>
2023-03-09 03:10:51 -0800

    add 654.py

---
+++ BinaryTree/654.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+from typing import Optional, List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
+        
+        def traversal(nums: List[int], begin: int, end: int) -> Optional[TreeNode]:
+            if begin >= end:
+                return None
+
+            max_index = begin
+            for index in range(begin, end):
+                if nums[index] > nums[max_index]:
+                    max_index = index
+            root = TreeNode(nums[max_index])
+            
+            root.left = traversal(nums, begin, max_index)
+            root.right = traversal(nums, max_index + 1, end)
+            return root
+        
+        if len(nums) == 0:
+                return None
+        return traversal(nums, 0, len(nums))
