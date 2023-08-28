c8c0dc95bbeba55b93cd39854b15e181df2ec071
Chence Shi <ascc594557067@gmail.com>
2023-08-27 22:13:27 -0700

    add 135

--- greedy/135.candy.py
+++ greedy/135.candy.py
@@ -7,6 +7,15 @@
 # @lc code=start
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        
+        candies = [1 for _ in range(len(ratings))]
+        print(candies)
+        for i in range(len(ratings) - 1):
+            if ratings[i] < ratings[i + 1]:
+                candies[i + 1] = candies[i] + 1
+        print(candies)
+        for i in range(len(ratings) - 2, -1, -1):
+            if ratings[i] > ratings[i + 1]:
+                candies[i] = max(candies[i + 1] + 1, candies[i])
+        print(candies)
+        return sum(candies)
 # @lc code=end
-
