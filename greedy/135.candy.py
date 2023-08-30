4e64e04a9c9c37dc51da2d26821e27dc80fe05f3
Chence Shi <ascc594557067@gmail.com>
2023-08-29 21:08:05 -0700

    update 135

--- greedy/135.candy.py
+++ greedy/135.candy.py
@@ -12,10 +12,10 @@ class Solution:
         for i in range(len(ratings) - 1):
             if ratings[i] < ratings[i + 1]:
                 candies[i + 1] = candies[i] + 1
-
-        for i in range(len(ratings) - 2, -1, -1):
+                
+        for i in reversed(range(len(ratings) - 1)):
             if ratings[i] > ratings[i + 1]:
                 candies[i] = max(candies[i + 1] + 1, candies[i])
-        
+
         return sum(candies)
 # @lc code=end
