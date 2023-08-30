cf96075ca04fe8e069c8977950a06c9ecb9841c9
Chence Shi <ascc594557067@gmail.com>
2023-08-30 14:19:34 -0700

    add 860

---
+++ greedy/860.lemonade-change.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=860 lang=python3
+#
+# [860] Lemonade Change
+#
+
+# @lc code=start
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five = 0
+        ten = 0
+        for bill in bills:
+            if bill == 5:
+                five += 1
+            if bill == 10:
+                if five <= 0:
+                    return False
+                five -= 1
+                ten += 1
+            if bill == 20:
+                if ten > 0 and five > 0:
+                    ten -= 1
+                    five -= 1
+                elif five >= 3:
+                    five -= 3
+                else:
+                    return False
+        return True
+
+# @lc code=end
