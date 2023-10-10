f6594864f3ce9a47a8408407907504220b57370b
Chence Shi <ascc594557067@gmail.com>
2023-10-10 01:38:56 -0700

    add rolling list for dp

--- DP/0-1-knapsack.py
+++ DP/0-1-knapsack.py
@@ -18,5 +18,22 @@ class Solution:
         print(dp)
         return dp[-1][-1]
 
+    def solve2(self)-> int:
+        weight = [1, 3, 4]
+        value = [15, 20, 30]
+        N = 4
+
+        dp = [0] * (N + 1)
+        # for i in range(weight[0], N + 1):
+        #     dp[i] = value[0]
+
+        for i in range(len(weight)):
+            for j in reversed(range(weight[i], N + 1)):
+                dp[j] = max(dp[j], dp[j - weight[i]] + value[i])
+
+        print(dp)
+        return dp[-1]
+
 s = Solution()
 s.solve()
+s.solve2()
