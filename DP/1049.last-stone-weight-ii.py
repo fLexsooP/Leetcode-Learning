49de271e865f9eb516e3e93a570092bcaa973857
Chence Shi <ascc594557067@gmail.com>
2023-10-18 01:21:46 -0700

    add 1049

---
+++ DP/1049.last-stone-weight-ii.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=1049 lang=python3
+#
+# [1049] Last Stone Weight II
+#
+
+# @lc code=start
+class Solution:
+    def lastStoneWeightII(self, stones: List[int]) -> int:
+        total = sum(stones)
+        target =  total // 2
+        dp = [0] * (target + 1)
+        for i in range(len(stones)):
+            for j in range(target, stones[i] - 1, -1):
+                dp[j] = max(dp[j], dp[j - stones[i]] + stones[i])
+
+        return (total - dp[target]) - dp[target]
+# @lc code=end
