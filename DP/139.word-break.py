becdb05a97d0a39bc93fec4c7e4e876c7072466d
Chence Shi <ascc594557067@gmail.com>
2023-11-05 01:14:48 -0700

    add 139

---
+++ DP/139.word-break.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=139 lang=python3
+#
+# [139] Word Break
+#
+
+# @lc code=start
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = [False] * (n + 1)
+        dp[0] = True
+
+        for i in range(1, n + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+
+        return dp[n]
+# @lc code=end
