8803c8be09a51d36115ccd291860db3d45028d63
Chence Shi <ascc594557067@gmail.com>
2023-11-13 01:42:58 -0800

    add dp sol for 337

--- DP/337.house-robber-iii.py
+++ DP/337.house-robber-iii.py
@@ -12,24 +12,44 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.mem = dict()
+    # def __init__(self):
+    #     self.mem = dict()
+
+    # def rob(self, root: Optional[TreeNode]) -> int:
+    #     if root is None:
+    #         return 0
+    #     if root in self.mem:
+    #         return self.mem[root]
+    #     # rob root
+    #     res1 = root.val
+    #     if root.left:
+    #         res1 += self.rob(root.left.left) + self.rob(root.left.right)
+    #     if root.right:
+    #         res1 += self.rob(root.right.left) + self.rob(root.right.right)
+
+    #     # not rob root
+    #     res2 = self.rob(root.left) + self.rob(root.right)
+
+    #     self.mem[root] = max(res1, res2)
+    #     return max(res1, res2)
 
     def rob(self, root: Optional[TreeNode]) -> int:
-        if root is None:
-            return 0
-        if root in self.mem:
-            return self.mem[root]
-        # rob root
-        res1 = root.val
-        if root.left:
-            res1 += self.rob(root.left.left) + self.rob(root.left.right)
-        if root.right:
-            res1 += self.rob(root.right.left) + self.rob(root.right.right)
+        def robTree(cur):
+            if cur is None:
+                return [0, 0]
 
-        # not rob root
-        res2 = self.rob(root.left) + self.rob(root.right)
+            left = robTree(cur.left)
+            right = robTree(cur.right)
 
-        self.mem[root] = max(res1, res2)
-        return max(res1, res2)
+            # rob current room
+            res1 = cur.val + left[0] + right[0]
+
+            # not rob current room
+            res2 = max(left[0], left[1]) + max(right[0], right[1])
+
+            # [not rob cur, rob cur]
+            return [res2, res1]
+
+        res = robTree(root)
+        return max(res)
 # @lc code=end
