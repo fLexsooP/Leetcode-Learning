607b9332614e2bb27fcd31fff97f1fad27afd0c4
Chence Shi <ascc594557067@gmail.com>
2024-03-26 02:10:59 -0700

    add 28 in java

---
+++ String/28.find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode id=28 lang=java
+ *
+ * [28] Find the Index of the First Occurrence in a String
+ */
+
+// @lc code=start
+class Solution {
+    public static int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
+// @lc code=end
