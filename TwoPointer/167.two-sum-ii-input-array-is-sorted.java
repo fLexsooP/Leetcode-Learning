16168524d9decac9a21a3f5e4c2bb5abcbb07b7b
Chence Shi <ascc594557067@gmail.com>
2024-04-03 01:39:10 -0700

    add 167

---
+++ TwoPointer/167.two-sum-ii-input-array-is-sorted.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=167 lang=java
+ *
+ * [167] Two Sum II - Input Array Is Sorted
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] res = new int[2];
+        int l = 0;
+        int r = numbers.length - 1;
+        while (l < r) {
+            if (numbers[l] + numbers[r] > target) {
+                r--;
+            } else if (numbers[l] + numbers[r] < target) {
+                l++;
+            } else {
+                res[0] = l + 1;
+                res[1] = r + 1;
+                break;
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
