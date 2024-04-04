8187a1dc285b1be2bf56cb37bcc260afca09edab
Chence Shi <ascc594557067@gmail.com>
2024-04-04 01:30:17 -0700

    add 3sum in java

---
+++ TwoPointer/15.3-sum.java
@@ -0,0 +1,47 @@
+
+/*
+ * @lc app=leetcode id=15 lang=java
+ *
+ * [15] 3Sum
+ */
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// @lc code=start
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int l = i + 1;
+            int r = nums.length - 1;
+
+            while (l < r) {
+                if (nums[i] + nums[l] + nums[r] > 0) {
+                    r--;
+                } else if (nums[i] + nums[l] + nums[r] < 0) {
+                    l++;
+                } else {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while (l < r && nums[l] == nums[l + 1]) {
+                        l++;
+                    }
+                    while (l < r && nums[r] == nums[r - 1]) {
+                        r--;
+                    }
+                    l++;
+                    r--;
+                }
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
