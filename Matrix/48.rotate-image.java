7a94e80186c7948eacacf3bc95ef12400bcfec60
Chence Shi <ascc594557067@gmail.com>
2024-04-25 01:32:34 -0700

    add 48

---
+++ Matrix/48.rotate-image.java
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=48 lang=java
+ *
+ * [48] Rotate Image
+ */
+
+// @lc code=start
+class Solution {
+    public void transpose(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+    }
+    public void swapColumn(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+        swapColumn(matrix);
+    }
+}
+// @lc code=end
