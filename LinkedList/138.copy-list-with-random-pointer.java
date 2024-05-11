7a0c1d7eceba7124e9ed056a198101a84cfcfda3
Chence Shi <ascc594557067@gmail.com>
2024-05-10 23:30:14 -0700

    add 138 good problem

---
+++ LinkedList/138.copy-list-with-random-pointer.java
@@ -0,0 +1,47 @@
+/*
+* @lc app=leetcode id=138 lang=java
+*
+* [138] Copy List with Random Pointer
+*/
+import java.util.HashMap;
+import java.util.Map;
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        
+        Map<Node, Node> map = new HashMap<>();
+        Node cur = head;
+        while (cur != null) {
+            map.put(cur, new Node(cur.val));
+            cur = cur.next;
+        }
+
+        cur = head;
+        while (cur != null) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+
+            cur = cur.next;
+        }
+        return map.get(head);
+    }
+}
+// @lc code=end
