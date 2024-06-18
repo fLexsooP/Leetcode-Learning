8b65544071a16ba3284d2a604e640c268dee6113
Chence Shi <ascc594557067@gmail.com>
2024-06-18 11:56:45 +0800

    add 129

---
+++ BinaryTree/129.sum-root-to-leaf-numbers.java
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=129 lang=java
+ *
+ * [129] Sum Root to Leaf Numbers
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int helper(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return sum * 10 + root.val;
+        }
+        int left = helper(root.left, sum * 10 + root.val);
+        int right = helper(root.right, sum * 10 + root.val);
+
+        return left + right;
+    }
+    public int sumNumbers(TreeNode root) {
+        return helper(root, 0);
+    }
+}
+// @lc code=end
