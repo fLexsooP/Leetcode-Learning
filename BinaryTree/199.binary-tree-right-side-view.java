78ec7a5b086dd7a173ae6533195e709fa0fbc077
Chence Shi <ascc594557067@gmail.com>
2024-07-08 23:14:06 +0800

    add 199

---
+++ BinaryTree/199.binary-tree-right-side-view.java
@@ -0,0 +1,55 @@
+/*
+ * @lc app=leetcode id=199 lang=java
+ *
+ * [199] Binary Tree Right Side View
+ */
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                TreeNode cur = queue.poll();
+                if (cur.left != null) {
+                    queue.add(cur.left);
+                }
+                if (cur.right != null) {
+                    queue.add(cur.right);
+                }
+                if (size == 1) {
+                    res.add(cur.val);
+                }
+                size--;
+            }
+        }
+        return res;
+    }
+}
+// @lc code=end
