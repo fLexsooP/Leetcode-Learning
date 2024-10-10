4336e585fd3a6e3526e262736ad7e799bee53cc6
Chence Shi <ascc594557067@gmail.com>
2024-10-10 07:32:04 -0700

    update 162

--- BinarySearchTree/162.find-peak-element.java
+++ BinarySearchTree/162.find-peak-element.java
@@ -7,30 +7,17 @@
 // @lc code=start
 class Solution {
     public int findPeakElement(int[] nums) {
-        int n = nums.length;
-        if (n == 1) {
-            return 0;
-        }
-        if (nums[0] > nums[1]) {
-            return 0;
-        }
-        if (nums[n - 1] > nums[n - 2]) {
-            return n - 1;
-        }
-
         int left = 0;
-        int right = n - 1;
-        while (left <= right) {
+        int right = nums.length - 1;
+        while (left < right) {
             int mid = left + ((right - left) / 2);
-            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
-                return mid;
-            } else if (nums[mid] > nums[mid - 1] && nums[mid] < nums[mid + 1]) {
-                left = mid;
-            } else {
+            if (nums[mid] > nums[mid + 1]) {
                 right = mid;
+            } else {
+                left = mid + 1;
             }
         }
-        return nums[left] > nums[right] ? left : right;
+        return left;
     }
 }
 // @lc code=end
