ee2c028f855b0faaafce8dd696151e317625290a
Chence Shi <ascc594557067@gmail.com>
2024-11-25 01:40:37 -0800

    add 67

---
+++ BitManipulation/67.add-binary.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=67 lang=java
+ *
+ * [67] Add Binary
+ */
+
+// @lc code=start
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+            carry = sum > 1 ? 1 : 0;
+            sb.append(sum % 2);
+        }
+        if (carry != 0) {
+            sb.append(carry);
+        }
+
+        return sb.reverse().toString();
+    }
+}
+// @lc code=end
