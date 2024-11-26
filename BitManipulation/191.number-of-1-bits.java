36dc2df5e69fabd46a7aaf4fc085891b2cc8e910
Chence Shi <ascc594557067@gmail.com>
2024-11-25 22:05:08 -0800

    add 191

---
+++ BitManipulation/191.number-of-1-bits.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=191 lang=java
+ *
+ * [191] Number of 1 Bits
+ */
+
+// @lc code=start
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                count++;
+            }
+            n /= 2;
+        }
+        return count;
+    }
+}
+// @lc code=end
